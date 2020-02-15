0f56008f46fe600ab8b787491e616ac4dd4c75e8
Sergio Ruiz <sergioruizdavila@gmail.com>
2020-02-14 21:26:21 -0500

    refactor: product-data in order to add an ID prop

--- src/app/interfaces/product-data.ts
+++ src/app/interfaces/product-data.ts
@@ -1,6 +1,7 @@
 export interface ProductData {
-  readonly code: string;
-  readonly name: string;
-  readonly price: number;
-  readonly urlImg: string;
+  id: number;
+  code: string;
+  name: string;
+  price: number;
+  urlImg: string;
 }
