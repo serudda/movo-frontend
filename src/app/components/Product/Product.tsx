0be035a9f3889e86b999d6a8a7782e399e88a299
Sergio Ruiz <sergioruizdavila@gmail.com>
2020-02-15 23:27:28 -0500

    refactor: add minus, plus and input change logic using the Checkout class on Product component

--- src/app/components/Product/Product.tsx
+++ src/app/components/Product/Product.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 
-import NumericStepper from '../NumericStepper/NumericStepper';
+import { CheckoutContext } from 'app/contexts/CheckoutContext';
 
 import { IProductData } from 'app/interfaces/product-data';
 
@@ -12,6 +12,40 @@ export interface Props {
 
 const Product = ({product}: Props) => {
   const {name, code, price, urlImg} = product;
+  const [value, setValue] = useState(0);
+  const [total, setTotal] = useState(0);
+  const checkout = useContext(CheckoutContext);
+
+  const handleMinusClick = () => {
+    if (value > 0) {
+      const newValue = value - 1;
+      setValue(newValue);
+      checkout.remove!(code);
+      setTotal(price * newValue);
+    }
+  };
+
+  const handlePlusClick = () => {
+    if (value < 10) {
+      const newValue = value + 1;
+      setValue(newValue);
+      checkout.scan!(code);
+      setTotal(price * newValue);
+    }
+  };
+
+  const handleValueChange = (e) => {
+    const newValue = e.target.value;
+    setValue(newValue);
+    checkout.removeAllProductsByCode!(code);
+
+    // TODO: Crashea cuando pongo 500000000 en el input. No es optimo esto. Buscar alternativa
+    for (let i = 0; i < newValue; i++) {
+      checkout.scan!(code);
+    }
+
+    setTotal(price * newValue);
+  };
 
   return (
     <li className="Product product row">
@@ -24,13 +58,17 @@ const Product = ({product}: Props) => {
           </div>
         </figure>
       </div>
-      <NumericStepper></NumericStepper>
+      <div className="col-quantity">
+        <button className="count" onClick={handleMinusClick}>-</button>
+        <input type="text" className="product-quantity" value={value} onChange={handleValueChange}/>
+        <button className="count" onClick={handlePlusClick}>+</button>
+      </div>
       <div className="col-price">
         <span className="product-price text-black text-base font-bold">{price}</span>
         <span className="product-currency currency font-bold ml-1">€</span>
       </div>
       <div className="col-total">
-        <span className="product-price text-black text-base font-bold">20</span>
+        <span className="product-price text-black text-base font-bold">{total}</span>
         <span className="product-currency currency font-bold ml-1">€</span>
       </div>
     </li>
